b92db6d9589ba479e927e5b23e3b0cb7ad873b09
inose-yuudai <inobee1412@yahoo.co.jp>
2023-03-30 04:04:31 +0900

    fetch templete

---
+++ src/firstindex.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+
+if(isset($_SESSION['id'])) {
+  header('Location: ../admin/auth/signin.php');
+  exit;
+}
+
+if(isset($_POST['submit'])) {
+  header('Location: ../admin/auth/signin.php');
+  exit;
+}
+print $_SESSION['id'];
+?>
+
+<form action="" method="post">
+  <input type="submit" name="submit" value="ログイン">
+</form>
